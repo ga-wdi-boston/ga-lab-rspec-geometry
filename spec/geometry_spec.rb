1b15f63cec1e8a3bbd4d3aa38c4558443b8f0c6a
Rahul Horé <hore.rahul@gmail.com>
2013-10-03 14:34:39 -0400

    Adds skeleton tests and module

--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,2 +1,25 @@
 require 'spec_helper'
 require_relative '../lib/geometry'
+
+describe "Geometry" do
+  it "should have a module called Geometry" do
+    
+  end
+
+  it "should have a class Triangle" do
+  end
+
+  it "should have a class Rectangle" do
+  end
+
+  it "should have a class Circle" do
+  end
+end
+
+describe "#Triangle" do
+  it "should have a method called perimeter" do
+  end
+
+  it "should have a method area" do
+  end
+end

c1e216d72bbe7f122bfacdfa3b136595d645786f
Amal Hussein <amalhussein08@gmail.com>
2013-10-03 15:04:37 -0400

    Added Perimeter Method to Triangle, and First Test

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -13,19 +13,34 @@
 # 			Area 
 
 #####################################
+require 'pry'
+
 module Geometry 
 
 class Triangle 
 	attr_accessor :sideA , :sideB, :sideC
 
+	def initialize(sideA, sideB, sideC)
+	 @sideA = sideA
+	 @sideB = sideB
+	 @sideC = sideC
+	end
 
+	def perimeter(sideA,sideB,sideC)
+		p = sideA * sideB * sideC
+	end 
 end 
-
 class Rectangle 
+	attr_accessor :length , :width 
+
 end 
 
 class Cirlce
+	attr_accessor :radius
+
 end 
 
 
 end 
+
+#binding.pry 

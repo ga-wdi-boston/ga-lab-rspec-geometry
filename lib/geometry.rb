a7404532884a43ecbfb33e7e99128668ff34eeac
Rahul Horé <hore.rahul@gmail.com>
2013-10-03 17:12:59 -0400

    Gets the perimeter and area tests to pass

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,19 +1,20 @@
 module Geometry
   class Triangle
-    attr_accessor :side1, :side2, :side3
+    attr_accessor :a, :b, :c
 
     def initialize(side1, side2, side3)
-      @side1 = side1
-      @side2 = side2
-      @side3 = side3
+      @a = side1
+      @b = side2
+      @c = side3
     end
 
-    def perimeter(side1, side2, side3)
-      side1 + side2 + side3
+    def perimeter
+      @a + @b + @c
     end
 
-    def area(side1, side2, side3)
-
+    def area
+      p = self.perimeter / 2
+      Math.sqrt(p * (p - @a) * (p - @b) * (p - @c))
     end
   end
 
--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,18 +1,37 @@
 require 'spec_helper'
 require_relative '../lib/geometry'
 
+describe Geometry::Triangle do
 
-describe Geometry::Triangle, "#perimeter" do
-  it "should have a method called perimeter" do
-    should respond_to :perimeter
+  let(:a) { 3 }
+  let(:b) { 4 }
+  let(:c) { 5 }
+  let(:perimeter) { 12 }
+
+  subject { Geometry::Triangle.new(a, b, c) }
+
+  
+
+  it "should have an area" do
+    subject.area.should eq 6.0
   end
+
+  its(:a) { should eq 3 }
+  its(:b) { should eq 4 }
+  its(:c) { should eq 5 }
+  
+  its(:perimeter) { should eq 12 }
+  # same as above
+  # it "should have a perimeter" do
+  #   subject.perimeter.should eq perimeter
+  # end
 end
 
-describe Geometry::Triangle, "#area" do
-  it "should have a method called area" do
-    should respond_to :area
-  end
-end
+# describe Geometry::Triangle, "#area" do
+#   it "should have a method called area" do
+#     should respond_to :area
+#   end
+# end
 
 
 # describe "#Triangle" do

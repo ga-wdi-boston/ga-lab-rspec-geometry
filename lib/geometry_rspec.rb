bf2ebd31f424b8cea645d25dd2fefd9caea7845a
Amal Hussein <amalhussein08@gmail.com>
2013-10-03 14:32:50 -0400

    Setup my Module and classes

---
+++ lib/geometry_rspec.rb
@@ -0,0 +1,31 @@
+########################################
+# module Geometry 
+# 		class Triangle 
+# 			Perimeter @sideA , @sideB, @sideC
+# 			Area
+
+# 		class Rectangle
+# 			Perimeter  @sideA, @sideB, @sideC
+# 			Area 
+
+# 		class Circle 
+# 			Circumference  @radius 
+# 			Area 
+
+#####################################
+module Geometry 
+
+class Triangle 
+	attr_accessor :sideA , :sideB, :sideC
+
+
+end 
+
+class Rectangle 
+end 
+
+class Cirlce
+end 
+
+
+end 

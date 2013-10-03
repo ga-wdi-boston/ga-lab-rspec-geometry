41b077b9f345af24d3219aef198e403857034474
Cori Allen <coriallen@gmail.com>
2013-10-03 15:23:32 -0400

    Creates first tests for geometry lab.

---
+++ lib/math.rb
@@ -0,0 +1,9 @@
+
+module Geometry
+	class Triangle
+		attr_accessor :side_a
+		def initialize(a, b, c)
+			@side_a = a
+		end
+	end
+end
---
+++ spec/math_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require_relative '../lib/math'
+
+describe Geometry::Triangle do
+	it "should have a side A" do
+	expect(method(@side_a))
+	end
+end

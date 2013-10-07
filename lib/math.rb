54dc1a4a5aa6093859e4ca8028d498b8b44f0d9f
Cori Allen <coriallen@gmail.com>
2013-10-07 07:16:21 -0400

    Adds working methods, working tests, at last.

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,62 @@
 
 module Geometry
 	class Triangle
-		attr_accessor :side_a
+		attr_accessor :side_a, :side_b, :side_c
 		def initialize(a, b, c)
 			@side_a = a
+			@side_b = b
+			@side_c = c
+		end
+		
+		def perimeter
+			@side_c + @side_b + @side_a
+		end
+		
+		def area
+			perimeter_half = (@side_c + @side_b + @side_a)/2
+			number = perimeter_half * ((perimeter_half-@side_a)*(perimeter_half-@side_b)*(perimeter_half-@side_c))
+			area = Math.sqrt(number)
+			return area
+		end
+
+		def triangle_valid?
+			if (@side_a+@side_b > @side_c)&& (@side_a+@side_c > @side_b) && (@side_b+@side_c > @side_a)
+				return true
+			else
+				return false
+			end
+		end
+	end
+
+	class Rectangle
+		attr_accessor :l, :w
+		def initialize(length, width)
+			@l = length
+			@w = width
+		end
+
+		def perimeter
+			(2 * @l) + (2 * @w)
+		end
+
+		def area
+			@l * @w
+		end
+
+	end
+
+	class Circle
+		attr_accessor :r
+		def initialize(radius)
+			@r = radius
+		end
+
+		def circumference
+			2 * @r * Math::PI
+		end
+
+		def area
+			Math::PI * (@r ** 2)
 		end
 	end
 end
--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -1,8 +1,43 @@
 require 'spec_helper'
 require_relative '../lib/math'
 
+### Tests from code-along with Tom ###
 describe Geometry::Triangle do
-	it "should have a side A" do
-	expect(method(@side_a))
-	end
+  
+  # set variables
+  let(:side_a) {3}
+  let(:side_b) {4}
+  let(:side_c) {5}
+
+  subject { Geometry::Triangle.new(side_a, side_b, side_c) }
+
+  its(:perimeter) {should eq 12}
+  its(:area) {should eq 6}
+  its(:triangle_valid?) {should eq true}
+
+end
+
+describe Geometry::Rectangle do
+  
+  # set variables
+  let(:l) {3}
+  let(:w) {4}
+
+  subject { Geometry::Rectangle.new(l, w) }
+
+  its(:perimeter) {should eq 14}
+  its(:area) {should eq 12}
+
+end
+
+describe Geometry::Circle do
+  
+  # set variables
+  let(:r) {3}
+
+  subject { Geometry::Circle.new(radius) }
+
+  let(:circumference) { Math::PI * 6}
+  let(:area) {Math::PI * 9}
+
 end

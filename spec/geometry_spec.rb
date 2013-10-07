4b3716441edde4899f980644405f73e41fee4857
Amal Hussein <amalhussein08@gmail.com>
2013-10-07 08:02:03 -0400

    All rspecs are working individually

--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 require_relative '../lib/geometry'
 
+######################################################TESTING FOR TRIANGLE 
 
 describe Geometry::Triangle do   
-	let(:a) {3}                        #here we are defining variables 
+	let(:a) {3}                        							#here we are defining variables 
 	let(:b) {4}
 	let(:c) {5}
 	let(:good_perimeter) {12}
@@ -14,15 +15,12 @@ subject {Geometry::Triangle.new(a,b,c)}
 
 	it "should be an instance of geometry Triangle" do 
 		subject.should be_an_instance_of Geometry::Triangle     #be_an_instance of is an rspec matcher
-	end 
+		end 
 
 	it "should have a side 'a' equal to 3" do 
 		subject.a.should eq 3 
-	end 
-
-	#its(:a){should eq 3} --> this is a shortcut for whats above 
-
-	it "should have a side 'b' equal to 3" do 
+		end 
+																#its(:a){should eq 3} --> this is a shortcut for whats above 	it "should have a side 'b' equal to 3" do 
 		subject.b.should eq 4 
 		end 
 
@@ -39,10 +37,10 @@ subject {Geometry::Triangle.new(a,b,c)}
 		end 
 end 
 
-#TESTING FOR RECTANGLE
+##########################################################TESTING FOR RECTANGLE
 
 describe Geometry::Rectangle do   
-	let(:a) {5}                                       #here we are defining variables 
+	let(:a) {5}                                     
 	let(:b) {7}
 	let(:good_perimeter) {24}
 	let (:good_area) {35}
@@ -57,8 +55,6 @@ subject {Geometry::Rectangle.new(a,b)}
 		subject.a.should eq 5 
 			end 
 
-	#its(:a){should eq 3} --> this is a shortcut for whats above 
-
 	it "should have a side 'b' equal to 7" do 
 		subject.b.should eq 7
 			end 
@@ -72,51 +68,30 @@ subject {Geometry::Rectangle.new(a,b)}
 			end 
 end 
 
+##########################################################TESTING FOR CIRCLE
 
-# describe Geometry::Rectangle do
-# 	let(:a) {5}
-# 	let(:b) {7}
-# 	let(:perimeter) {24}
-# 	let (:area) {35}
+describe Geometry::Circle do
+	let(:a) {5}
+	let(:area) {78.5398 }
+	let(:circumference) {31.41592653589793}
 
-# #testing out attr_accessor
-# #call a method named a on the subject 
-# 	its(:a) {should eq 5}
-# 	its(:b) {should eq 7}
-# 	its(:perimeter) {should eq 24}
-# 	its(:area) {should eq 35}
+subject {Geometry::Circle.new(a)}
 
-# subject {Geometry::Rectangle.new(a,b)}
+it "should be an instance of geometry Circle" do 
+		subject.should be_an_instance_of Geometry::Circle
+			end
+	
+	it "should have a radius 'a'" do 
+		subject.a.should eq 5
+		end 
+	
+	it "should have an area" do
+	subject.area.should eq 78.5398  
+		end 
 
-# 	it "should have a perimeter" do
-# 	subject.perimeter.should eq perimeter 
-# 		end 
-
-# 	it "should have an area" do
-# 	subject.area.should eq area 
-# 		end 
-# end 
-
-# ##############################TESTING FOR CIRCLE###########
-# describe Geometry::Circle do
-# 	let(:a) {5}
-# 	let(:area) {78.5398 }
-# 	let(:circumference) {31.41592653589793}
-
-# 	its(:a) {should eq 5}
-# 	its(:circumference) {should eq 31.41592653589793}
-# 	its (:area) {should eq 78.5398}
-
-
-# subject {Geometry::Circle.new(a)}
-
-# 	it "should have an area" do
-# 	subject.area.should eq area 
-# 		end 
-
-# 	it "should have a circumference" do
-# 	subject.circumference.should eq circumference 
-# 		end 
-# end
+	it "should have a circumference" do
+	subject.circumference.should eq 31.41592653589793 
+		end 
+end
 
 

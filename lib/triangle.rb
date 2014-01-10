972c338b51e8d3a7a684a08d45f05af691dc8543
tsyan <anna.tsykalova@gmail.com>
2014-01-10 14:52:25 -0500

    made triangle perimeter method

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,23 @@
 module Geometry # container for classes
-	class Triangle # adds this class to any existing Geometry module, doesn't overwrite existing classes
+	class Triangle
+
+		attr_accessor :adjacent, :opposite, :hypotenuse
+
+		def initialize(adjacent, opposite, hypotenuse)
+			@adjacent = adjacent
+			@opposite = opposite
+			@hypotenuse = hypotenuse
+		end
+
+		# calculate the perimeter
+		def perimeter
+			@adjacent + @opposite + @hypotenuse
+		end
+
+		# # calculate the area
+		# def area
+		# 	@length * @width
+		# end
+
 	end
 end
-
-Geometry::Triangle.new
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,10 +1,20 @@
 require 'spec_helper'
 require_relative '../lib/triangle'
 
-# describe Geometry::Triangle do
-# 	describe '#perimeter' do
-# 		it 'adds the lengths of the sides' do
-# 			#
-# 		end
-# 	end
-# end
+describe Geometry::Triangle do
+
+	describe '#perimeter' do
+		it 'adds the lengths of the sides' do
+			triangle = Geometry::Triangle.new(3,4,5)
+			expect(triangle.perimeter).to eq 3+4+5
+		end
+	end
+
+	# describe '#area' do
+	# 	it 'multiplies length by width' do
+	# 		triangle = Geometry::Triangle.new(4,5)
+	# 		expect(triangle.area).to eq 4*5
+	# 	end
+	# end
+
+end

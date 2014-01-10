23a17e43758737f7998462aa2f7c22a9be284463
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-10 15:16:35 -0500

    Triangle Area complete

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,20 @@
 module Geometry
 	class Triangle
 
-		def initialize(sideA, sideB, sideC)
+		def initialize(sideA, sideB, sideC, base, height)
 			@sideA = sideA
 			@sideB = sideB
 			@sideC = sideC
+			@base = base
+			@height = height
 		end
 
 		def perimeter
 			triangle_perimeter = @sideA + @sideB + @sideC
+		end
+
+		def area
+			triangle_area = 0.5 * @base * @height
 		end
 	end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -4,8 +4,23 @@ require_relative '../lib/triangle'
 describe Geometry::Triangle do
 	describe '#perimeter' do
 		it 'adds the lengths of the sides' do
-			triangle1 = Geometry::Triangle.new(3, 4, 5)
+			triangle1 = Geometry::Triangle.new(3, 4, 5, 4, 5)
 			expect(triangle1.perimeter).to eq 12
+		end
+		it 'adds the lengths of the sides' do
+			triangle2 = Geometry::Triangle.new(10, 11, 12, 11, 12)
+			expect(triangle2.perimeter).to eq 33
+		end
+	end
+
+	describe '#area' do
+		it 'calculates the area of a triangle' do
+			triangle3 = Geometry::Triangle.new(3, 4, 5, 4, 5)
+			expect(triangle3.area).to eq 10
+		end
+		it 'calculates the area of a triangle' do
+			triangle4 = Geometry::Triangle.new(10, 11, 12, 11, 12)
+			expect(triangle4.area).to eq 66
 		end
 	end
 end

f4435c105fd56e76996ca49a4ebb207b5c4f2586
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-10 15:56:25 -0500

    Circle Circumference and Area complete

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,4 +1,16 @@
 module Geometry
 	class Circle
+
+		def initialize(radius)
+			@radius = radius
+		end
+
+		def circumference
+			circumference = 2.0 * Math::PI * @radius
+		end
+
+		def area
+			area = Math::PI * @radius * @radius
+		end
 	end
 end

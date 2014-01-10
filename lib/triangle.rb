e86659be0ee882a6300a13ad9e45075faad6c625
Matt Higgins <higginsmt@gmail.com>
2014-01-10 17:26:38 -0500

    Cool, I think I got it working

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,30 +1,50 @@
-# The Triangle will have instance variables of @sideA, @sideB and @sideC. Triangle needs methods to calculate the perimeter, area, angles (outputs an array of angles), and valid?.
+
+require 'pry'
 
 module Geometry
 
 	class Triangle
 		attr_accessor :sideA, :sideB, :sideC
 
-		def initialize
+		def initialize(sideA, sideB, sideC)
 			@sideA = sideA
 			@sideB = sideB
 			@sideC = sideC
 		end
 
-		def perim
+		def perimeter
+			@sideA + @sideB + @sideC
 		end
 
 		def area
+			(sideA * sideB) / 2
+		end
+
+		def find_angle(a, b,c)
+			a = a.to_f
+			b = b.to_f
+			c = c.to_f
+
+			Math.acos(((a**2) + (b**2) -(c**2)) / (2*a*b)) * 180 / Math::PI
 		end
 
 		def angles
+			array_of_angles = []
+
+			array_of_angles << find_angle(@sideB, @sideC, @sideA)  # Side A
+			array_of_angles << find_angle(@sideC, @sideA, @sideB)  # Side B
+			array_of_angles << find_angle(@sideA, @sideB, @sideC) # Side C
+
 		end
 
-		def angles
-			array_of_angles = {}
+		def valid?
+			a = @sideA
+			b = @sideB
+			c = @sideC
+
+			(a + b > c) && (a + c > b) && (b + c > a)
+
 		end
+
 	end
 end
-
-
-Geometry::Triangle.new
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,13 +1,35 @@
+# The Triangle will have instance variables of @sideA, @sideB and @sideC. Triangle needs methods to calculate the perimeter, area, angles (outputs an array of angles), and valid?.
+
 require 'spec_helper'
 require_relative '../lib/triangle.rb'
 
 describe Geometry::Triangle do
-	describe '#length' do
-		it 'adds the lengths of the sides' do
-			triangle = Geometry
-		end
+
+	it 'calculates the perimeter' do
+		triangle = Geometry::Triangle.new(3,4,5)
+		expect(triangle.perimeter).to eq 12
 	end
-	describe
+
+	it 'calculates the area' do
+		triangle = Geometry::Triangle.new(3,4,5)
+		expect(triangle.area).to eq 6
+	end
+
+	it 'returns an array of angles' do
+	 	triangle = Geometry::Triangle.new(3,4,5)
+	 	expect(triangle.angles.class).to eq Array
+	 end
+
+	 it 'returns the correct angles in the array' do
+	 	triangle = Geometry::Triangle.new(3,3,3)
+	 	expect(triangle.angles).to eq [59.99999999999999, 59.99999999999999, 59.99999999999999]
+	 end
+
+	it 'is a valid triangle' do
+		triangle = Geometry::Triangle.new(3,4,5)
+		expect(triangle.valid?).to eq true
+	end
+
 end
 
 

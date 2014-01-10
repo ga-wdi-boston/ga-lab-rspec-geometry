502445f8475d53ddc7c09f384a93b7e427dd2ac5
Jordan Morano <jordan.morano@gmail.com>
2014-01-10 16:29:38 -0500

    rectangle perimeter passes

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,4 +1,15 @@
 module Geometry
+
   class Rectangle
+    attr_accessor :length, :width
+
+    def initialize(length, width)
+      @length = length
+      @width = width
+    end
+
+    def perimeter
+      @length * 2 + @width * 2
+    end
   end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,28 @@
 require 'spec_helper'
 require_relative '../lib/rectangle'
+
+
+describe Geometry::Rectangle do
+  describe '#perimeter' do
+    it 'returns perimeter of the rectangle' do
+      rect = Geometry::Rectangle.new(2,4)
+      expect(rect.perimeter).to eq 12
+    end
+
+    it 'returns perimeter of the rectangle' do
+      rect = Geometry::Rectangle.new(3,5)
+      expect(rect.perimeter).to eq 16
+    end
+
+  end
+
+  describe '#area' do
+    it 'returns the area of a rectangle' do
+      rect = Geometry::Rectangle.new(2,4)
+      expect(rect.area).to eq 8
+    end
+
+  end
+
+
+end

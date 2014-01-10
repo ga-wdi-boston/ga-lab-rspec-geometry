5f8c298905267f0fe6f16a12d68781826f0fc7cd
Jack Henley <jack.d.henley@gmail.com>
2014-01-10 16:47:38 -0500

    circle functions finished

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,5 +1,17 @@
 module Geometry
 	class Circle
 
+		def initialize(radius)
+			@radius = radius
+		end
+
+		def area
+			Math::PI * @radius ** 2
+		end
+
+		def circumfrence
+			Math::PI * @radius * 2
+		end
+
 	end
 end
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,2 +1,26 @@
 require 'spec_helper'
 require_relative '../lib/circle.rb'
+
+describe Geometry::Circle do
+
+
+
+	describe '#area' do
+
+		it 'returns the area of the circle' do
+
+			circle = Geometry::Circle.new(3)
+			expect(circle.area).to eq Math::PI * 3 ** 2
+		end
+	end
+
+	describe '#circumfrence' do
+
+		it 'returns the circumfrence of the circle' do
+
+			circle = Geometry::Circle.new(3)
+			expect(circle.circumfrence).to eq Math::PI * 3 * 2
+		end
+	end
+
+end

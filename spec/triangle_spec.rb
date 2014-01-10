bc112f1158c7a6b80634ebcc4a1bac7f550e94a2
Janice Smith <janice.k.n.smith@gmail.com>
2014-01-10 17:00:12 -0500

    now angle finding method for triangles works

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -13,6 +13,7 @@ describe Geometry::Triangle do
 			expect(test_tri.perimeter).to eq 7
 		end
 	end
+
 	describe '#area_tri' do
 		it 'calc area' do
 			test_tri_2 = Geometry::Triangle.new
@@ -22,6 +23,20 @@ describe Geometry::Triangle do
 			test_tri_2.side_c = 2.0
 
 			expect(test_tri_2.area_tri).to eq 1.7320508075688772
+		end
+	end
+
+	describe '#angle_finder' do
+		it 'finds angles' do
+			test_tri_3 = Geometry::Triangle.new
+
+			test_tri_3.side_a = 3.0
+			test_tri_3.side_b = 3.0
+			test_tri_3.side_c = 3.0
+
+			expected_array = [59.99999999999999, 59.99999999999999, 60.000000000000014]
+
+			expect(test_tri_3.angle_finder).to eq expected_array
 		end
 	end
 end

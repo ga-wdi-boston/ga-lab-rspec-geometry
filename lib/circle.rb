3637e172b75a8bace80f1686be3c8eb5f2628dda
Jordan Morano <jordan.morano@gmail.com>
2014-01-10 16:39:42 -0500

    defined Circle#circumference and passed

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,4 +1,13 @@
 module Geometry
   class Circle
+    attr_accessor :radius
+
+    def initialize(radius)
+      @radius = radius
+    end
+
+    def circumference
+      (2 * Math::PI * @radius).round(4)
+    end
   end
 end
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,2 +1,24 @@
 require 'spec_helper'
 require_relative '../lib/circle'
+
+describe Geometry::Circle do
+  describe '#circumference' do
+    it 'returns the circumference of a circle' do
+      circle = Geometry::Circle.new(2)
+      expect(circle.circumference).to eq 12.5664
+    end
+
+    it 'returns the circumference of a circle' do
+      circle = Geometry::Circle.new(5)
+      expect(circle.circumference).to eq 31.4159
+    end
+  end
+
+  describe '#area' do
+    it 'returns the area of a circle' do
+      circle = Geometry::Circle.new(5)
+      expect(circle.circumference).to eq 78.5398
+    end
+  end
+
+end

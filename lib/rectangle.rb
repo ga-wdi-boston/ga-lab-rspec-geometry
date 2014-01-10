53fa881ede8ee8eb58980f08193c3c06c33cf5da
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-10 15:28:40 -0500

    Rectangle Perimeter complete

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,4 +1,15 @@
 module Geometry
 	class Rectangle
+
+		def initialize(sideA, sideB, sideC, sideD)
+			@sideA = sideA
+			@sideB = sideB
+			@sideC = sideC
+			@sideD = sideD
+		end
+
+		def perimeter
+			rectangle_perimeter = @sideA + @sideB + @sideC + @sideD
+		end
 	end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,15 @@
 require 'spec_helper'
 require_relative '../lib/rectangle'
+
+describe Geometry::Rectangle do
+	describe '#perimeter' do
+		it 'adds the lengths of the sides' do
+			rect1 = Geometry::Rectangle.new(7, 8, 8, 7)
+			expect(rect1.perimeter).to eq 30
+		end
+		it 'adds the lengths of the sides' do
+			rect2 = Geometry::Rectangle.new(12, 12, 12, 12)
+			expect(rect2.perimeter).to eq 48
+		end
+	end
+end

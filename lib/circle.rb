69a3793c180dc7c3c9e03691221d0c09543ac1d7
Mike Stone <mikestone14@gmail.com>
2014-01-10 16:09:08 -0500

    Created a circumference method for Circles with passing tests

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,5 +1,11 @@
 module Geometry
 	class Circle
+		def initialize(radius)
+			@radius = radius.to_f
+		end
 
+		def circumference
+			(2 * @radius) * Math::PI
+		end
 	end
 end
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,2 +1,11 @@
 require 'spec_helper'
-require_relative '../lib/circle'
+require_relative '../lib/circle.rb'
+
+describe Geometry::Circle do
+	describe '#circumference' do
+		it 'should calculate the circumference of a circle' do
+			circle = Geometry::Circle.new(2)
+			expect(circle.circumference).to eq 12.566370614359172
+		end
+	end
+end

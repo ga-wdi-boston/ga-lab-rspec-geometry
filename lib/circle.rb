7a423db388bd34d91ef13b38e4308134da7da606
Mike Stone <mikestone14@gmail.com>
2014-01-10 16:12:57 -0500

    Created an area method for Circles with passing tests

--- lib/circle.rb
+++ lib/circle.rb
@@ -7,5 +7,10 @@ module Geometry
 		def circumference
 			(2 * @radius) * Math::PI
 		end
+
+		def area
+			(@radius ** 2) * Math::PI
+		end
 	end
 end
+
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -8,4 +8,11 @@ describe Geometry::Circle do
 			expect(circle.circumference).to eq 12.566370614359172
 		end
 	end
+
+	describe '#area' do
+		it 'should calculate the area of a circle' do
+			circle = Geometry::Circle.new(3)
+			expect(circle.area).to eq 28.274333882308138
+		end
+	end
 end

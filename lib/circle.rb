ef2516b8a5b82c1ce942c4dc87ef292192300aae
Alex Grant <alex@grantovich.net>
2014-01-10 22:17:38 -0500

    Add circumference method for circles

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,4 +1,11 @@
 module Geometry
   class Circle
+    def initialize(radius)
+      @radius = radius
+    end
+
+    def circumference
+      2 * Math::PI * @radius
+    end
   end
 end
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,2 +1,17 @@
 require 'spec_helper'
 require_relative '../lib/circle'
+
+describe Geometry::Circle do
+  describe '#circumference' do
+    it 'returns the circumference of the circle' do
+      first_circle = Geometry::Circle.new(5)
+      second_circle = Geometry::Circle.new(47)
+
+      # Turns out there's a matcher that doesn't force you to get all the
+      # decimal places exactly right! Exercise: How could we apply this to
+      # our Triangle spec?
+      expect(first_circle.circumference).to be_within(0.001).of(31.415)
+      expect(second_circle.circumference).to be_within(0.001).of(295.309)
+    end
+  end
+end

e12e2fb1c87525292cadc24daeb101674091a2c5
Alex Grant <alex@grantovich.net>
2014-01-10 21:10:08 -0500

    Add initializer and perimeter method for triangles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,11 @@
 module Geometry
   class Triangle
+    def initialize(sideA, sideB, sideC)
+      @sideA, @sideB, @sideC = sideA, sideB, sideC
+    end
+
+    def perimeter
+      @sideA + @sideB + @sideC
+    end
   end
 end
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 require_relative '../lib/triangle'
+
+describe Geometry::Triangle do
+  describe '#perimeter' do
+    it 'returns the perimeter of the triangle' do
+      first_triangle = Geometry::Triangle.new(1, 1, 1)
+      second_triangle = Geometry::Triangle.new(2, 4, 5)
+
+      expect(first_triangle.perimeter).to eq 3
+      expect(second_triangle.perimeter).to eq 11
+    end
+  end
+end

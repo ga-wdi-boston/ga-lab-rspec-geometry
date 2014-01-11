6dd739adef76bd762a688c4317d9f8cbe1261123
Alex Grant <alex@grantovich.net>
2014-01-10 21:57:15 -0500

    Add rectangle initializer and perimeter method

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -1,4 +1,16 @@
 module Geometry
   class Rectangle
+    def initialize(length, width)
+      # Multiple assignment! We can put several variables on the left side and
+      # an equal number on the right side, separated by commas, and they will
+      # be assigned in order.
+      @length, @width = length, width
+    end
+
+    def perimeter
+      # Note: Ruby respects order of operations to some extent, so parentheses
+      # are not needed here. But they are useful for clarifying larger formulas.
+      @length * 2 + @width * 2
+    end
   end
 end
--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,2 +1,14 @@
 require 'spec_helper'
 require_relative '../lib/rectangle'
+
+describe Geometry::Rectangle do
+  describe '#perimeter' do
+    it 'returns the perimeter of the rectangle' do
+      first_rectangle = Geometry::Rectangle.new(2, 5)
+      second_rectangle = Geometry::Rectangle.new(3, 7)
+
+      expect(first_rectangle.perimeter).to eq 14
+      expect(second_rectangle.perimeter).to eq 20
+    end
+  end
+end

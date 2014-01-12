369a52236ffb94a951f8de0f5e77cb5012cb6bf9
Praggya Rustagi Barretto <praggya@gmail.com>
2014-01-11 20:13:22 -0500

    updated triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,12 +18,34 @@ module Geometry
 			triangle_area = 0.5 * @base * @height
 		end
 
-		def cosine_a
-			cos_a = ((@sideB ** 2) + (@sideC ** 2) - (@sideA ** 2)) / (2 * @sideB * @sideC)
+		def angle_a
+			cos_a = ((@sideB**2) + (@sideC**2) - (@sideA**2)) / (2*@sideB*@sideC)
+			angle_a = ((Math.acos*cos_a)*(180/Math::PI).abs
 		end
 
-		def angle_a
-			angle_a = Math::acos * cos_a
+		def angle_b
+			cos_b = ((@sideC**2) + (@sideA**2) - (@sideB**2)) / (2*@sideA*@sideC)
+			angle_b = ((Math.acos*cos_b)*(180/Math::PI)).abs
+		end
+
+		def angle_c
+			angle_c = 180 - angle_a - angle_b
+		end
+
+		def angle_array
+			angle_array = []
+			angle_array << angle_a
+			angle_array << angle_b
+			angle_array << angle_c
+		end
+
+		def valid_triangle
+			if
+				(@sideA + @sideB > sideC) && (@sideB + @sideC > @sideA) && (@sideA + @sideC > @sideB)
+				return true
+			else
+				return false
+			end
 		end
 	end
 end

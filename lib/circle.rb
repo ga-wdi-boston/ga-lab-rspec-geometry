89d087a5d070f93ce681859a9bf6e029ca87f046
My Name <rlbell@alum.bu.edu>
2014-01-12 20:30:18 -0500

    Added rspec Test for Circle, Rectangle area, perimeter/circumference

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,4 +1,18 @@
+
 module Geometry
 	class Circle
-	end
+
+		def initialize(radius)
+			@radius = radius
+		end
+
+		def area
+			circle_area = (Math::PI) * (@radius * 2)
+		end
+
+    def circumference
+		  diameter = @radius * 2
+		  circle_circumference = (Math::PI) * diameter
+    end
+  end
 end
